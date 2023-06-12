e637e55aba668984d8c9c75658bf43f405dae3cd
s4llm4n <s4llm4n@gmail.com>
2023-06-12 14:26:22 +0700

    middleware admin, integration dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -32,14 +32,16 @@
                                             @if ($checkout->is_paid)
                                                 <span class="badge bg-success">Paid</span>
                                             @else
-                                            <span class="badge bg-warning">waiting</span>
+                                                <span class="badge bg-warning">waiting</span>
                                             @endif
                                         </td>
                                         <td>
-                                            <form action="" method="POST">
+                                            @if (!$checkout->is_paid)
+                                            <form action="{{route('admin.checkout.update', $checkout->id)}}" method="POST">
                                                 @csrf
                                                 <button class="btn btn-primary btn-sm">Set to Paid</button>
-                                            </form>
+                                            </form>                                                
+                                            @endif
                                         </td>
                                     </tr>
                                 @empty 
